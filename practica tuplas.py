589f3189ee1a76a146b3db2af1de39bb1c445de7
Alejovc <106875554+Alejovc@users.noreply.github.com>
2022-06-04 11:07:53 -0500

    Add files via upload

---
+++ practica tuplas.py
@@ -0,0 +1,6 @@
+mitupla=("Alejandro", 13, 5,2004)
+nombre, dia , mes, agno=mitupla
+print(nombre)
+print(dia)
+print(mes)
+print(agno)
---
+++ practicas_listas2.py
@@ -0,0 +1,5 @@
+miLista=["Maria", 5, True, 78.5]*3
+
+
+
+print(miLista[:])
